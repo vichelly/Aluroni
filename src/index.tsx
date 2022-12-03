b74c617b9f52cc4174b341b847808b4d616e1302
Vitor <vlucasff@hotmail.com>
2022-12-03 11:57:55 -0300

    att

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Cardapio from './pages/Botao';
+import Cardapio from 'pages/Cardapio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
